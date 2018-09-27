c070344fea85a028c3c96d43a249c5f0610d9d19
h.ono <h.ono@frevo-works.co.jp>
2018-09-27 10:31:10 +0900

    update

--- sw.js
+++ sw.js
@@ -4,15 +4,15 @@ importScripts('https://frevo-works-ono.github.io/nuxt-onsen-app/workbox.dev.678c
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/app.d14ed07667aa4d9c04a9.js",
-    "revision": "5138acc675ad4936ff81fd1818c59a8d"
+    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/app.95b23c2aade2181ef95e.js",
+    "revision": "679b75fc24e942b5328df4b6b94425f6"
   },
   {
     "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/layouts/default.1982f6824d40f4f8a06b.js",
     "revision": "72bb4830210e43996978f53770a47c88"
   },
   {
-    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/manifest.f5c44d5d7ff707334102.js",
+    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/manifest.5b1a9afc93f6b9da3c3a.js",
     "revision": "8b5061f06519473241693ca90dd5965a"
   },
   {
@@ -37,7 +37,7 @@ workbox.skipWaiting()
 
 workbox.routing.registerRoute(new RegExp('https://frevo-works-ono.github.io/nuxt-onsen-app/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('https://frevo-works-ono.github.io/nuxt-onsen-app/.*'), workbox.strategies.networkFirst({}), 'GET')
 
 
 
