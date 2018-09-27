de807d2f51be58f00493082ef766f02ebe1941ed
h.ono <h.ono@frevo-works.co.jp>
2018-09-27 10:25:58 +0900

    update

--- sw.js
+++ sw.js
@@ -4,15 +4,15 @@ importScripts('https://frevo-works-ono.github.io/nuxt-onsen-app/workbox.dev.678c
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/app.3b0ed5ac3c45d900ba56.js",
-    "revision": "78c9bacabd71259b5dbc9d7e51518819"
+    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/app.d14ed07667aa4d9c04a9.js",
+    "revision": "5138acc675ad4936ff81fd1818c59a8d"
   },
   {
     "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/layouts/default.1982f6824d40f4f8a06b.js",
     "revision": "72bb4830210e43996978f53770a47c88"
   },
   {
-    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/manifest.b88cf1f6e6a3d3011c51.js",
+    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/manifest.f5c44d5d7ff707334102.js",
     "revision": "8b5061f06519473241693ca90dd5965a"
   },
   {
