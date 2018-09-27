ea5b5da915adad99493292f31245d100d354a71f
h.ono <h.ono@frevo-works.co.jp>
2018-09-27 10:36:52 +0900

    update

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-importScripts('https://frevo-works-ono.github.io/nuxt-onsen-app/workbox.dev.678c6960.js')
+importScripts('/nuxt-onsen-app/_nuxt/workbox.dev.678c6960.js')
 
 
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/app.95b23c2aade2181ef95e.js",
-    "revision": "679b75fc24e942b5328df4b6b94425f6"
+    "url": "/nuxt-onsen-app/_nuxt/app.8dca9619d4c29025a9df.js",
+    "revision": "fd90ecb9f2d561e8f745c6df1ce9b35d"
   },
   {
-    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/layouts/default.1982f6824d40f4f8a06b.js",
+    "url": "/nuxt-onsen-app/_nuxt/layouts/default.1982f6824d40f4f8a06b.js",
     "revision": "72bb4830210e43996978f53770a47c88"
   },
   {
-    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/manifest.5b1a9afc93f6b9da3c3a.js",
-    "revision": "8b5061f06519473241693ca90dd5965a"
+    "url": "/nuxt-onsen-app/_nuxt/manifest.ac6fa45cd6f250c5a56e.js",
+    "revision": "c75a4da0c9f2936b20e176b4ba186699"
   },
   {
-    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/pages/index.b0dbcf1dc5110e3cd099.js",
+    "url": "/nuxt-onsen-app/_nuxt/pages/index.b0dbcf1dc5110e3cd099.js",
     "revision": "cf8b4f5ccafc30815a16bb3c14e5fc2b"
   },
   {
-    "url": "https://frevo-works-ono.github.io/nuxt-onsen-app/vendor.1777aa0fc406c7778bb1.js",
+    "url": "/nuxt-onsen-app/_nuxt/vendor.1777aa0fc406c7778bb1.js",
     "revision": "180c5b78b9de299b9706c4dfd722c190"
   }
 ], {
@@ -35,9 +35,9 @@ workbox.clientsClaim()
 workbox.skipWaiting()
 
 
-workbox.routing.registerRoute(new RegExp('https://frevo-works-ono.github.io/nuxt-onsen-app/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/nuxt-onsen-app/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('https://frevo-works-ono.github.io/nuxt-onsen-app/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/nuxt-onsen-app/.*'), workbox.strategies.networkFirst({}), 'GET')
 
 
 
